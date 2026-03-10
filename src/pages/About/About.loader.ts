0a3ed392c3e439e6691e86b3d4d46cd193af851a
Ignacio <185011694+nimbusphagia@users.noreply.github.com>
2026-03-09 21:18:15 -0500

    add author page and fix footer

--- src/pages/About/About.loader.ts
+++ src/pages/About/About.loader.ts
@@ -1,4 +1,11 @@
-
+import { apiClient } from "../../apiClient";
+import type { User } from "../../types/User";
+const AUTHOR_ID = import.meta.env.VITE_NIMBUS_AUTHOR_ID;
 export async function aboutLoader() {
-
+  try {
+    const author = await apiClient<User>(`/public/users/${AUTHOR_ID}`);
+    return { author };
+  } catch (error) {
+    throw error;
+  }
 }
--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,8 +1,39 @@
+import { useLoaderData } from 'react-router-dom';
 import s from './About.module.css'
 export function AboutPage() {
+  const { author } = useLoaderData();
   return (
     <div
       className={s.body}
-    ></div>
+    >
+      <div
+        className={`blueCard ${s.about}`}
+      >
+
+        <article
+          className={s.article}
+        >
+          <header
+            className={s.aHeader}
+          >
+            <img
+              src={author.imgUrl}
+              alt="" />
+          </header>
+          <div
+            className={s.veil}
+          >
+            <div className={`dashedCard ${s.main}`}>
+              <h1>
+                {`Hi, I'm ${author.name}`}
+              </h1>
+              <p>
+                {author.description}
+              </p>
+            </div>
+          </div>
+        </article>
+      </div>
+    </div>
   )
 }
